b6ddc2e5446d15e2b9a4ea09d4fd0e758336c77e
Florin Iacob <florin.e.iacob@gmail.com>
2017-01-15 22:29:57 +0200

    Add Certified host + loading GIF

--- app/scripts/directives/loading_page.js
+++ app/scripts/directives/loading_page.js
@@ -15,7 +15,7 @@ app.directive('loadingPage', function() {
               displayMessage = '<label class="text-center" style="width:100%;font-style:italic;">' + attrs.message + '</label>';
             }
             var htmlText = '<div class="container" style="height:100%;width:100%;padding:40px;">' +
-                '<img class="center-block" ng-src="images/loading.gif" alt="Loading ..." style="width:' + attrs.width + ';"></img>' +
+                '<img class="center-block" ng-src="images/logo/logoGif_300x300.gif" alt="Loading ..." style="width:' + attrs.width + ';"></img>' +
                 displayMessage +
             '</div>';
             element.replaceWith(htmlText);
